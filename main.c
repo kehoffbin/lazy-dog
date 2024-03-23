afc4c3905d027a08423e528d45e9eba8792fde11
hkerstyn <>
2024-03-23 13:53:10 +0100

    scratch again

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "hash.h"
-#include <stdio.h>
-
-int main() {
-    struct HashTable hashTable = make_hash_table();
-    push_h(&hashTable, "Hallo", 3);
-    push_h(&hashTable, "Welt", 6);
-    printf("%d", find(&hashTable, "Hallo"));
-}
