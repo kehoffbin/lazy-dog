6e0dff0a7daedf9e2d5bed94f36d0ad3e610573b
hkerstyn <>
2024-05-20 15:48:57 +0200

    restructuer

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ f:close()
 function value(node)
 	return file:sub(node.i, node.j - 1)
 end
+
 tokens = tuple(file, 1) >> fn(loop)(parse_token)
 for _, token in ipairs(tokens.elems) do
 	print(token.type, value(token))
